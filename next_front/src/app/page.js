588f549b831928313c7bbe403706d94f419a99e2
Babatunde Adeoti <babaolu98@gmail.com>
2024-05-10 12:49:48 +0100

    Setting up cover page

--- next_front/src/app/page.js
+++ next_front/src/app/page.js
@@ -1,12 +1,25 @@
 import Image from "next/image";
 import "./main.css";
 import Link from "next/link";
+import frontCover from "/public/images/Cover1.jpg";
 
 export default function Home() {
   return (
     <main>
-      <h1>See a visualize representation of your data</h1>
-      <h3><Link href="/dashboard/">Start plotting</Link></h3>
+      <div className="landing"></div>
+      <div className="describe">
+      <h1>Get a visualized representation of your data</h1>
+      <h3>Either with your file...</h3>
+      <h4>Or you Generate it.</h4>
+      <h2><Link href="/dashboard/">Start plotting!</Link></h2>
+      <Image
+        src={frontCover}
+	width={560}
+	height={360}
+	alt="Example plot"
+	className="coverImage"
+	/>
+      </div>
     </main>
   );
 }
